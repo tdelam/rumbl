32d4dcc1d9de92b51a396b4947a46fb06db81f07
Trevor Delamorandiere <tdelam@gmail.com>
2019-12-16 16:23:50 -0500

    Added registration - next up, write a plug for auth

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -1,6 +1,7 @@
 defmodule RumblWeb.UserController do
   use RumblWeb, :controller
 
+  alias Rumbl.Accounts.User
   alias Rumbl.Accounts
 
   def index(conn, _params) do
@@ -12,4 +13,20 @@ defmodule RumblWeb.UserController do
     user = Accounts.get_user(id)
     render(conn, "show.html", user: user)
   end
-end
+
+  def new(conn, _params) do
+    changeset = Accounts.change_registration(%User{}, %{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    case Accounts.register_user(user_params) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: Routes.user_path(conn, :index))
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+end 
