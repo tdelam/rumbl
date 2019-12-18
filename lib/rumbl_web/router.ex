ecaf511648b06a0e5b364dcf58f9371307910b2a
Trevor Delamorandiere <tdelam@gmail.com>
2019-12-18 14:36:26 -0500

    Login and logout with auth plug and controllers

--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -7,6 +7,7 @@ defmodule RumblWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug RumblWeb.Auth
   end
 
   pipeline :api do
@@ -18,5 +19,6 @@ defmodule RumblWeb.Router do
 
     get "/", PageController, :index
     resources "/users", UserController, only: [:index, :show, :new, :create]
+    resources "/sessions", SessionController, only: [:new, :create, :delete]
   end
 end
