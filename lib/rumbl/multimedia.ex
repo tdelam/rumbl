c191f8e7a84c4a91514fddc7be3377c351534792
Trevor Delamorandiere <tdelam@gmail.com>
2019-12-20 15:14:11 -0500

    Locked down videos

---
+++ lib/rumbl/multimedia.ex
@@ -0,0 +1,53 @@
+defmodule Rumbl.Multimedia do
+  @moduledoc """
+  The Multimedia context.
+  """
+
+  import Ecto.Query, warn: false
+  alias Rumbl.Repo
+  alias Rumbl.Accounts
+  alias Rumbl.Multimedia.Video
+
+  def list_user_videos(%Accounts.User{} = user) do
+    Video
+    |> user_videos_query(user)
+    |> Repo.all()
+  end
+
+  def get_user_video!(%Accounts.User{} = user, id) do
+    Video
+    |> user_videos_query(user)
+    |> Repo.get!(id)
+  end
+
+  def list_videos do
+    Repo.all(Video)
+  end
+
+  def get_video!(id), do: Repo.get!(Video, id)
+
+  def create_video(%Accounts.User{} = user, attrs \\ %{}) do
+    %Video{}
+    |> Video.changeset(attrs)
+    |> Ecto.Changeset.put_assoc(:user, user)
+    |> Repo.insert()
+  end
+
+  def update_video(%Video{} = video, attrs) do
+    video
+    |> Video.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_video(%Video{} = video) do
+    Repo.delete(video)
+  end
+
+  def change_video(%Video{} = video) do
+    Video.changeset(video, %{})
+  end
+
+  defp user_videos_query(query, %Accounts.User{id: user_id}) do
+    from(v in query, where: v.user_id == ^user_id)
+  end
+end
